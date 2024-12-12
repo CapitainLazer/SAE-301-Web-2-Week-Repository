8ee1a98b0b19e1b06b82febd8f717be650132caa
CapitainLazer <romaric.lazer33@gmail.com>
2024-12-12 11:03:16 +0100

    Update Index.php
    déploiement dans une nouvelle branche

--- Index.php
+++ Index.php
@@ -4,8 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Accueil</title>
+    <link rel="stylesheet" href="./CSS/style.css" type="text/css">
 </head>
 <body>
+
     
 </body>
 </html>
