872827b2a046bbd895d1eb9e5596972d43080023
Virginien17 <Virginien.BACHER@etu.uca.fr>
2024-12-12 10:48:29 +0100

    Update header.php

--- addon/header.php
+++ addon/header.php
@@ -8,7 +8,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Mumo</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-        <link src="css/styles.css" rel="stylesheet">
+        <link src="CSS/style.css" rel="stylesheet">
         <link rel="icon" type="image/png" href="images/favicon-mumo.png">
     </head>
 
