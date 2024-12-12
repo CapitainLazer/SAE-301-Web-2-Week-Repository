8bf8c9a66db6f101e8eafca95c2439f6c4cf9656
CapitainLazer <romaric.lazer33@gmail.com>
2024-12-12 13:56:13 +0100

    Update formulaire_de_contact.php
    mise a jour temporaire du formulaire de contact

--- addon/formulaire_de_contact.php
+++ addon/formulaire_de_contact.php
@@ -1,8 +1,14 @@
-<html>
+<html lang="fr">
 <h1>Contactez-nous</h1>
     <form method="post">
-        <label>Votre email</label>
+        <label>Nom</label>
+        <input type="text" name="nom" required placeholder="NOM">
+        <label>Prenom</label>
+        <input type="text" name="nom" required placeholder="PRENOM">
+        <label>Email</label>
         <input type="email" name="email" required>
+        <label>Téléphone</label>
+        <input type="" name="" required>
         <label>Message</label>
         <textarea name="message" required></textarea>
         <input type="submit">
