abe3dd1a8105165664fcd43fca408eab7ef4d05d
AntoinePlane-chefProj <antoineplane2@gmail.com>
2024-12-12 15:45:15 +0100

    Page contacts
    J'ai ajouté le header + commencé à mettre en forme le formulaire de contact avec Bootstrap + CSS

--- Contact.php
+++ Contact.php
@@ -3,24 +3,32 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="/CSS/style.css">
+    <title>Nous Contacter</title>
 </head>
 <body>
-<h1>Contactez-nous</h1>
-    <form method="post">
-        <label>Nom</label>
-        <input type="text" name="nom" required placeholder="NOM">
-        <label>Prenom</label>
-        <input type="text" name="nom" required placeholder="PRENOM">
-        <label>Email</label>
-        <input type="email" name="email" required>
-        <label>Téléphone</label>
-        <input type="" name="" required>
-        <label>Message</label>
-        <textarea name="message" required></textarea>
-        <input type="submit">
-    </form>
+
+<?php //inclus le Header
+include('addon/header.php');
+?>
+
+<!--Le Formulaire de contacts-->
+<section class="container rounded" style="background-color: #D63542;">
+    <h1>Contactez<stlyle style='font-family: Arial;'>-</stlyle>nous</h1> <!--Modification de lapolice pour le tiret " - " -->
+        <form method="post" class="row d-flex flex-row">
+            <label>Nom</label>
+            <input type="text" name="nom" required placeholder="NOM">
+            <label>Prenom</label>
+            <input type="text" name="nom" required placeholder="PRENOM">
+            <label>Email</label>
+            <input type="email" name="email" required>
+            <label>Téléphone</label>
+            <input type="" name="" required>
+            <label>Message</label>
+            <textarea name="message" required></textarea>
+            <input type="submit" class="btn btn-danger">
+        </form>
+</section>
+
 <?php
 ?>
 </body>
