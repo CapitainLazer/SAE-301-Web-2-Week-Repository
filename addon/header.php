9c78bdfd81365ac85dbb6f5f63c23d2cb5deb6b4
Virginien17 <Virginien.BACHER@etu.uca.fr>
2024-12-12 14:39:17 +0100

    header et footer

--- addon/header.php
+++ addon/header.php
@@ -1,34 +1,36 @@
-</header>
-</body>
-</html>
-<!DOCTYPE html>
-<html lang="fr">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Mumo</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-        <link src="CSS/style.css" rel="stylesheet">
-        <link rel="icon" type="image/png" href="images/favicon-mumo.png">
-    </head>
-
-    <body>
-        <!-- Navbar -->
-        <nav class="navbar navbar-expand-lg navbar-dark">
-            <div class="container">
-                <a class="logo" href="index.html"><img src="./images/mumo.png"></a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav ms-auto">
-                        <li class="nav-item"><a class="nav-link" href="index.html" id="red">Accueil</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#">Evènements Calendrier</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#">MuMo x Pompidou</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#">Pédagogique</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#">À Propos</a></li>
-                        <div class="user-icon">👤</div>
-                    </ul>
-                </div>
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="CSS/style.css" rel="stylesheet" type="text/css">
+</head>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<!-- Navbar -->
+<header>
+    <nav class="navbar navbar-expand-lg navbar-custom">
+        <div class="container-fluid ">
+        <a class="logo" href="index.php"><img src="./images/logo_MUMO.png"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse " id="navbarTogglerDemo01">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0 nav m-auto ">
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php">Accueil</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="horraire.html">Evènements Calendrier</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#collections">MuMo x Pompidou</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#infos">Pédagogique</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#infos">À Propos</a>
+                    </li>
+                    <div class="user-icon">👤</div>
+                </ul>
             </div>
-        </nav>
+        </div>
+    </nav>
+</header>
