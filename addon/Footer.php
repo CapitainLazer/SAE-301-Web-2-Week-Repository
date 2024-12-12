cab826c7c7a8d96c9eeba2299bbb81a0e1c11cfe
Virginien17 <Virginien.BACHER@etu.uca.fr>
2024-12-12 10:46:43 +0100

    Footer.php

---
+++ addon/Footer.php
@@ -0,0 +1,18 @@
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="CSS/style.css" rel="stylesheet" type="text/css">
+</head>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<footer class="footer py-5">
+    <div class="container">
+        <div class="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top"></div>
+            <div class="row">
+                <ul class="nav flex-row">
+                    <a href="#" class="nav-link p-0">Accueil</a>
+                    <a href="#" class="nav-link p-0">Horaires des séances</a>
+                    <a href="#" class="nav-link p-0">Notre cinéma</a>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
