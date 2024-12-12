dbafc24689aa4a4986307fc06d8c2311e0b951f3
Anysia251 <anysia.fanguin@etu.uca.fr>
2024-12-12 10:00:48 +0100

    Create header.php
    Ce code correspond au header du site. Il inclut le logo, qui renvoie à la page d'accueil, ainsi qu'un menu permettant d'accéder aux autres pages.

---
+++ addon/header.php
@@ -0,0 +1,34 @@
+</header>
+</body>
+</html>
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Mumo</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link src="css/styles.css" rel="stylesheet">
+        <link rel="icon" type="image/png" href="images/favicon-mumo.png">
+    </head>
+
+    <body>
+        <!-- Navbar -->
+        <nav class="navbar navbar-expand-lg navbar-dark">
+            <div class="container">
+                <a class="logo" href="index.html"><img src="./images/mumo.png"></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link" href="index.html" id="red">Accueil</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#">Evènements Calendrier</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#">MuMo x Pompidou</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#">Pédagogique</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#">À Propos</a></li>
+                        <div class="user-icon">👤</div>
+                    </ul>
+                </div>
+            </div>
+        </nav>
