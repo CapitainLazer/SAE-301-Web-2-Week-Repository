661adf1abf5ae19a055621f652335769d5ad8f35
CapitainLazer <romaric.lazer33@gmail.com>
2024-12-12 11:29:35 +0100

    formulaire de contact
    création d'un formulaire de contact

---
+++ addon/formulaire_de_contact.php
@@ -0,0 +1,13 @@
+<html>
+<h1>Contactez-nous</h1>
+    <form method="post">
+        <label>Votre email</label>
+        <input type="email" name="email" required>
+        <label>Message</label>
+        <textarea name="message" required></textarea>
+        <input type="submit">
+    </form>
+<?php
+
+?>
+</html>
