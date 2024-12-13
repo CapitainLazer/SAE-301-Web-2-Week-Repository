c54015e0f4322eebf35aed6b73700ba62c5f47f0
AntoinePlane-chefProj <antoineplane2@gmail.com>
2024-12-13 12:02:52 +0100

    formulaire contact MAJ
    J'ai poursuivi mon travail d'intégration du formulaire et de la page contact.
    Travail pratiquement terminé mais bug avec le background-image + faire en sorte de récupérer les données du formulaire

--- Contact.php
+++ Contact.php
@@ -11,51 +11,66 @@
 include_once('addon/header.php');
 ?>
 
-<main>
-    <!--Le Formulaire de contacts-->
-    <section id="zoneFormulaire" class="container rounded" style="background-color: #D63542;">
-        <h1>Contactez<stlyle style='font-family: Arial;'>-</stlyle>nous</h1> <!--Modification de la police pour le tiret " - " -->
-            <form method="post" class="row d-flex flex-row p-3">
+<main style="position: relative;" class="decoImg">
+    <!--<img src="images/camion_MuMo.png" style="width: 100vw; filter: brightness(0.4); z-index: -1;"></img>-->
 
-                <div>
+    <!--Le Formulaire de contacts-->
+    <section id="zoneFormulaire" class="container rounded">
+        <h1>Contactez<stlyle style='font-family: Arial;'>-</stlyle>nous</h1> <!--Modification de la police pour le tiret " - " -->
+            <form method="post" class="row d-flex flex-column p-3">
+
+                <div class="col-5 my-3">
                     <label>Nom</label>
                     <br>
-                    <input type="text" name="nom" required placeholder="NOM">
+                    <input class="w-100" type="text" name="nom" required placeholder="Nom">
                 </div>
 
-                <div>
+                <div class="col-5 my-3">
                     <label>Prenom</label>
                     <br>
-                    <input type="text" name="nom" required placeholder="PRENOM">
+                    <input class="w-100" type="text" name="nom" required placeholder="Prénom">
                 </div>
 
-                <div>
+                <div class="col-5 my-3">
                     <label>Email</label>
                     <br>
-                    <input type="email" name="email" required>
+                    <input class="w-100" type="email" name="email" required placeholder="votreemail@mail.com">
                 </div>
 
-                <div>
+                <div class="col-5 my-3">
                     <label>Téléphone</label>
                     <br>
-                    <input type="" name="" required>
+                    <input class="w-100" type="" name="" required>
                 </div>
 
-                <div>
-                    <label>Téléphone</label>
+                <div class="col-5 my-3">
+                    <label>Date</label>
                     <br>
-                    <input type="" name="" required>
+                    <input class="w-100" type="date" name="" required>
                 </div>
 
-                <div>
+                <div class="col-8 my-3">
                     <label>Message</label>
                     <br>
-                    <textarea name="message" required></textarea>
+                    <textarea class="w-100" name="message" required></textarea>
                 </div>
 
-                <div>
-                    <input type="submit" class="btn btn-danger">
+                <div class="col-8 my-3 d-flex flex-row gap-3">
+                    <input type="checkbox" name="message" required></textarea>
+                    <p style="color: white; font-size: 12px;">En soumettant ce formulaire, j'accepte que mes données personnelles soient utilisées à des fins commerciales. 
+                    Pour en savoir plus sur l'utilisation de vos données, consultez notre <em>RGPD et Politique de confidentialité</em>.</p>
                 </div>
+
+                <div class="d-flex flex-row gap-3">
+                    <div class="col-3 my-3">
+                        <input type="submit" value="Envoyer" class="boutonForm">
+                    </div>
+
+                    <div class="col-3 my-3">
+                        <input type="button" value="Vider" class="boutonForm">
+                    </div>
+                </div>
+
 
             </form>
     </section>
