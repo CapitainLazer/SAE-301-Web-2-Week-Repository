78945390c8369912f04062c2648f59511dfffc8d
Zoé Coutant <zoe.coutant.basket.56@gmail.com>
2024-12-13 10:47:26 +0100

    Création du calendrier
    Création du calendrier avec FullCalendar

---
+++ API/calendrier.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="./style.css" rel="stylesheet" type="text/css"/>
+        <link href="https://cdn.jsdelivr.net/npm/fullcalendar@6.1.8/main.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/fullcalendar@6.1.15/index.global.min.js"></script>
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <script src="./calendrier.js" type="text/javascript"></script>
+        <title>Évènements</title>
+    </head>
+    <body>       
+        <h2>Évènements</h2>
+        <div id="calendar"></div>
+    </body>
+</html>
