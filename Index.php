53bf82ec08a04897e849af4ad45d847c35a15246
Anysia251 <anysia.fanguin@etu.uca.fr>
2024-12-13 15:11:17 +0100

    modification du html de la page index

--- Index.php
+++ Index.php
@@ -18,15 +18,15 @@
 
 <!-- Section 1 : Image et Texte du Camion -->
 <section class="content">
-    <div class="row">
-        <div class="col-12 col-md-6">
+    <div class="row align-items-center">
+        <div class="col-12 col-sm-12 col-lg-6 col-md-6 d-flex flex-colomn align-items-center">
             <div class="camion">
-                <img src="images/camion.webp" class="img-fluid" alt="Image des collections permanentes au musée" />
+                <img src="images/camion.webp" class="img-fluid rounded" style="height:470px; object-fit: cover;" alt="Image des collections permanentes au musée" />
             </div>
         </div>
-        <div class="col">
-            <div class="texte">
-                <p>
+        <div class="col-12 col-sm-12 col-lg-6 col-md-6 ">
+            <div class="texte ">
+                <p class="">
                     Le MuMo (Musée Mobile) est un musée itinérant créé pour rendre l'art contemporain accessible à tous, notamment aux populations éloignées des institutions culturelles traditionnelles. Conçu à l'origine par Ingrid Brochard en 2011, le MuMo s'installe dans des camions transformés en espaces d'exposition. Il se déplace dans des écoles, des quartiers ruraux ou urbains, et offre une expérience artistique immersive.
                     Les œuvres présentées, souvent réalisées par des artistes contemporains renommés, couvrent divers médiums comme la peinture, la sculpture, la photographie ou l'installation. L'objectif est de démocratiser l'art, stimuler la curiosité et éveiller la sensibilité artistique, en particulier chez les enfants. Grâce à son format nomade, le MuMo transcende les frontières géographiques et sociales pour promouvoir l'art comme un bien commun.
                 </p>
@@ -76,33 +76,33 @@
         </div>
         <p>Plus qu’un partenariat...</p>
         <section class="content">
-            <div class="row">
-                <div class="col-12 col-md-6">
-                    <div class="texte">
+            <div class="row align-items-center">
+                <div class="col-12 col-lg-6  col-md-12 col-sm-12 d-flex flex-colomn align-items-center">
+                    <div class="texte mr-30">
                         <p>
                             Le MuMo et le Centre Pompidou collaborent pour rendre l'art moderne et contemporain accessible au plus grand nombre. Cette association permet au MuMo de présenter des œuvres emblématiques issues des collections du Centre Pompidou dans ses espaces itinérants.
                             L’objectif principal est de toucher des publics éloignés des grandes institutions culturelles, notamment en zones rurales ou dans des quartiers moins desservis. Grâce à cette collaboration, des œuvres d'art majeures voyagent directement auprès des communautés, accompagnées d'ateliers pédagogiques et d'animations pour approfondir l'expérience artistique. Ce partenariat incarne une démarche innovante de démocratisation culturelle, combinant la mobilité du MuMo et le prestige du Centre Pompidou.
                         </p>
                     </div>
                 </div>
-                <div class="col">
+                <div class="col-12 col-lg-6  col-md-12 col-sm-12">
                     <div class="collection-info">
-                        <img src="images/mumo.webp" class="img-fluid" alt="Image des collections permanentes au musée" />
+                        <img src="images/mumo.webp" class="img-fluid rounded" alt="Image des collections permanentes au musée" />
                     </div>
                 </div>
             </div>
-            <div class="bouton">
+           
+        </section>
+             <div class="bouton">
                 <button class="h">Voir plus</button>
             </div>
-        </section>
     </div>
 </div>
 
 <!-- Section 4 : À propos -->
 <section class="section">
-    <div class="hr-with-text">
-        <hr class="custom-hr">
-        <h2>À propos</h2>
+    <div>
+        <h2>A propos</h2>
     </div>
     <p>Quelques-unes de nos œuvres.</p>
     <div class="about-images">
