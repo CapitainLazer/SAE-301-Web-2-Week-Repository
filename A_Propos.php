18fd46e6b7452b2462d1664671af9fc67680a08e
AntoinePlane-chefProj <antoineplane2@gmail.com>
2024-12-14 18:25:26 +0100

    page A Propos
    J'ai poursuivi mon travail sur la page "A Propos.php" pour ajouter les membres de l'équipe du musée + des oeuvres

--- A_Propos.php
+++ A_Propos.php
@@ -10,6 +10,88 @@
 </head>
 <body>
     <?php include_once('addon/header.php');?>
+
+    <div class="content">
+        <!-- Section : L'équipe du Musée -->
+            <p>
+                Né de la conviction que l'art est un outil d'ouverture, de partage et d'éveil au monde, MuMo est un musée mobile destiné 
+                à aller à la rencontre des enfants, en particulier ceux qui se trouvent éloignés de l'accès à la culture,
+                en se rendant directement sur leur lieu de vie : cour d'école, centre de loisirs, parking d'un quartier...<br>
+                Le Musée Mobile prend la forme d'un container qui peut aisément voyager par bateau, avant d'être chargé sur un camion. 
+                Parvenu à destination, il se transforme en un musée, ouvrant sur quatre espaces distincts, 
+                chacun plongeant les enfants dans un univers différent : peinture, sculpture, installation, vidéo, design, etc.<br>
+                Lancé sur les routes entre 2011 et 2016, ce musée itinérant a parcouru 7 pays d'Europe et d'Afrique 
+                pour proposer aux enfants de 6 à 12 ans une expérience inédite de l'art contemporain, à travers la découverte 
+                d'oeuvres créées spécialement par une vingtaine d'artistes de la scène internationale, qui explorent 
+                la thématique sociale du «Vivre ensemble».<br>
+                Une enquête a été menée suite au passage du MuMo en France entre 2011 et 2013. Après 2 ans de tournée, 
+                MuMo a offert à 56% des enfants accueillis leur première visite d'un musée, à travers une expérience marquante, 
+                qui renouvelle l'intérêt pour l'art. Pour voir les résultats complets de l'enquête, cliquer ici.<br><br>
+                Après cette première version aménagée par l'architecte Adam Kalkin dans un conteneur en 2011, 
+                un nouveau musée itinérant voit le jour avec la designer matali crasset en 2017, 
+                pour diffuser les oeuvres des collections du Cnap et des Frac. Un troisième camion-musée, le MuMo x Centre Pompidou, 
+                est imaginé en 2022 par Hérault Arnod Architectures et l'artiste Krijn de Koning pour faire circuler les oeuvres du Centre.
+            </p>
+
+        <section class="container">
+            <h1>L'equipe du MuMo</h1>
+            <br>
+            <div class="row d-flex flex-row">
+                <div class="card" style="width: 18rem;"><!--div pour un membre de l'équipe-->
+                    <img src="./images/equipeMusee/ingrid_brochard.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">Ingrid BROCHARD | Fondatrice du MuMo</p>
+                    </div>
+                </div>
+
+                <div class="card" style="width: 18rem;"><!--div pour un membre de l'équipe-->
+                    <img src="./images/equipeMusee/rudy_boublil.webp" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">Rudy BOUBLIL | Président</p>
+                    </div>
+                </div>
+
+                <div class="card" style="width: 18rem;"><!--div pour un membre de l'équipe-->
+                    <img src="./images/equipeMusee/jonathan_gervoson.webp" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">Jonathan GERVOSON | Secrétaire</p>
+                    </div>
+                </div>
+
+                <div class="card" style="width: 18rem;"><!--div pour un membre de l'équipe-->
+                    <img src="./images/equipeMusee/lucie_avril.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">Lucie AVRIL | Directrice opérationnelle</p>
+                    </div>
+                </div>
+
+                <div class="card" style="width: 18rem;"><!--div pour un membre de l'équipe-->
+                    <img src="./images/equipeMusee/pp_parDefaut.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">Arno DEVO | Directeur artistique</p>
+                    </div>
+                </div>
+
+                <div class="card" style="width: 18rem;"><!--div pour un membre de l'équipe-->
+                    <img src="./images/equipeMusee/louise_harrewyn.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">Louise HARREWYN | Responsable opérationnelle & Contact MuMo x Centre Pompidou</p>
+                    </div>
+                </div>
+
+                <div class="card" style="width: 18rem;"><!--div pour un membre de l'équipe-->
+                    <img src="./images/equipeMusee/clemence_renaud.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">Clémence RENAUD | Coordinatrice du MuMo dédié aux Frac</p>
+                    </div>
+                </div>
+
+            </div>
+            
+        </section>
+    </div>
+
+    
     
     <!-- Section : Chiffres Clés --  ! pas dans la div "content" car pas de padding pour le fond rouge ! -->
     <section class="container" style="background: red;">
