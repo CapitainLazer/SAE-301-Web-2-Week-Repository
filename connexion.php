46d0b83a34cdbc15aa1ee5375dda07c9ec70850e
CapitainLazer <romaric.lazer33@gmail.com>
2024-12-16 14:20:32 +0100

    Version HTML / JS final pour connexion

--- connexion.php
+++ connexion.php
@@ -1,55 +1,57 @@
 <!DOCTYPE html>
 <html lang="fr">
-  <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="./CSS/style.css" rel="stylesheet" type="text/css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <title>Connexion</title>
-    <link rel="icon" type="image/png" href="images/favicon_MuMo.png">
-  </head>
   <body>       
         <?php include_once('./addon/header.php'); ?>
         <section class="section_connexion">
-            <div class="tab-header">
-                <a class="tab-link active" data-ref="connexion" href="javascript:void(0)">Connexion</a>
-                <a class="tab-link" data-ref="inscription" href="javascript:void(0)">Inscription</a>
-            </div>
-            <div class="tab-body" data-id="connexion">
+            <div class="tab-body connexion" id="connexion">
                 <h2>Connexion a votre compte</h2>
-                <form action="verifconnexion.php" method="POST" class="form-container">
+                <form action="verifconnexion.php" method="POST"
+class="form-container">
                     <div class="form-group">
-                        <label for="mail">Mail*</label>
-                        <input type="email" id="mail" name="mail" required placeholder="nom.prenom@mail.com">
+                        <label for="mail">Mail</label>
+                        <input type="email" id="mail" name="mail"
+required placeholder="nom.prenom@mail.com">
                     </div>
                     <div class="form-group">
                         <label for="mdp">Mot de Passe*</label>
-                        <input type="password" id="mdp" name="mdp" required placeholder="mot de passe">
+                        <input type="password" id="mdp" name="mdp"
+required placeholder="mot de passe">
                     </div>
                     <button type="submit" class="btn-submit">Connexion</button>
                 </form>
             </div>
-            <div class="tab-body" data-id="inscription">
+            <div class="tab-body hidden inscription" id="inscription">
                 <h2>Creation de votre compte</h2>
                 <form action="___.php" method="POST" class="form-container">
                     <div class="form-group">
                         <label for="mail">Mail*</label>
-                        <input type="email" id="mail" name="mail" required placeholder="nom.prenom@mail.com">
+                        <input type="email" id="mail" name="mail"
+required placeholder="nom.prenom@mail.com">
                     </div>
                     <div class="form-group">
                         <label for="mdp">Mot de Passe*</label>
-                        <input type="password" id="createmdp" name="mdp" required placeholder="mot de passe">
+                        <input type="password" id="createmdp"
+name="mdp" required placeholder="mot de passe">
                     </div>
                     <div class="form-group">
                         <label for="mdp">Confirmez le Mot de Passe*</label>
-                        <input type="password" id="confirmmdp" name="mdp" required placeholder="mot de passe">
+                        <input type="password" id="confirmmdp"
+name="mdp" required placeholder="mot de passe">
                     </div>
-                    <button type="submit" class="btn-submit">Inscription</button>
+                    <button type="submit"
+class="btn-submit">Inscription</button>
                 </form>
             </div>
+            <br>
+            <div class="tab-header">
+                <a class="link-connexion hidden">Connexion</a>
+                <a class="link-inscription">Inscription</a>
+            </div>
+            <br>
         </section>
         <br>
+        <script src="./JS/Formulaire_connection.js" defer type="text/javascript"></script>
         <?php include_once('./addon/Footer.php'); ?>
+        
     </body>
 </html>
