22d566a78d443efb03af4fbebdf622e9e34cdc9a
CapitainLazer <romaric.lazer33@gmail.com>
2024-12-16 14:57:12 +0100

    Update verifconnexion.php

--- verifconnexion.php
+++ verifconnexion.php
@@ -7,11 +7,19 @@
     $admin=$resultats->fetch(PDO::FETCH_ASSOC);
     $resultats->closeCursor();
 
-    if ($_POST["mail"]==$admin["mail"] && $_POST["mdp"]==$admin["mdp"]) {
+    $requete2='SELECT mail, mdp FROM `compte`';
+    $resultats2=$bdd->query($requete2);
+    $compte=$resultats2->fetch(PDO::FETCH_ASSOC);
+    $resultats2->closeCursor();
+
+    if ($_POST["mail"]==$admin["mail"] && $_POST[sha1("mdp")]==$admin[sha1("mdp")]) {
         header('Location: ./admin/PageAdmin.php');
         exit();
     }
-
+    if ($_POST["mail"]==$compte["mail"] && $_POST[sha1("mdp")]==$compte[sha1("mdp")]) {
+        header('Location: ./user/PageUser.php');
+        exit();
+    }
     else {
         header('Location: index.php');
         exit();
