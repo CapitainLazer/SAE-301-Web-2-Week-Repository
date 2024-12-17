391cd941b59eeaa1c440d3a74172fad903849973
Anysia251 <anysia.fanguin@etu.uca.fr>
2024-12-17 11:45:44 +0100

    ajout popup d'erreur

---
+++ JS/popup_1.js
@@ -0,0 +1,9 @@
+  // Fonction pour afficher une pop-up
+function showPopup() {
+    alert("Adresse e-mail invalide.");
+}
+  
+// Attacher la fonction à un événement de clic sur un bouton
+document.getElementById("myButton").addEventListener("click", showPopup);
+  
+  
---
+++ JS/popup_2.js
@@ -0,0 +1,8 @@
+// Fonction pour afficher la pop-up
+function showPopup() {
+    alert("Les mots de passe ne correspondent pas.");
+  }
+  
+  // Exemple d'événement pour afficher la pop-up
+  document.getElementById("myButton").addEventListener("click", showPopup);
+  
